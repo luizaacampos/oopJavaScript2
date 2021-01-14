3952fa6d24a9432f7b76b29054007811f22c7431
Luiza Campos <luiza.almcampos@gmail.com>
2021-01-14 10:35:01 -0300

    refactored

---
+++ assets/scripts/App/ProjectItem.js
@@ -0,0 +1,54 @@
+import { DOMHelper } from '../Utility/DOMHelper.js'
+
+export class ProjectItem {
+    hasActiveTooltip = false
+
+    constructor(id, updateProjectsListsFunction, type) {
+        this.id = id
+        this.updateProjectsListsHandler = updateProjectsListsFunction
+        this.connectSwitchButton(type)
+        this.connectMoreInfoButton()
+        this.connectDrag()
+    }
+
+    showMpreInfoHandler() {
+        if (this.hasActiveTooltip) {
+            return
+        }
+        const projectElement = document.getElementById(this.id)
+        const tooltipText = projectElement.dataset.extraInfo
+        import('./Tooltip.js').then(module => {
+            const tooltip = new module.Tooltip(() => {
+                this.hasActiveTooltip = false
+            }, tooltipText, this.id)
+            tooltip.attach()
+            this.hasActiveTooltip = true
+        })
+    }
+
+    connectDrag() {
+        document.getElementById(this.id).addEventListener('dragstart', event => {
+            event.dataTransfer.setData('text/plain', this.id)
+            event.dataTransfer.effectAllowed = 'move'
+        })
+    }
+
+    connectMoreInfoButton() {
+        const projectItemEl = document.getElementById(this.id)
+        let moreInfoButton = projectItemEl.querySelector('button:first-of-type')
+        moreInfoButton.addEventListener('click', this.showMpreInfoHandler.bind(this))
+    }
+
+    connectSwitchButton(type) {
+        const projectItemEl = document.getElementById(this.id)
+        let switchButton = projectItemEl.querySelector('button:last-of-type')
+        switchButton = DOMHelper.clearEventListeners(switchButton)
+        switchButton.textContent = type === 'active' ? 'finish' : 'activate'
+        switchButton.addEventListener('click', this.updateProjectsListsHandler.bind(null, this.id))
+    }
+
+    update(updateProjectListsFn, type) {
+        this.updateProjectListsHandler = updateProjectListsFn
+        this.connectSwitchButton(type)
+    }
+}
